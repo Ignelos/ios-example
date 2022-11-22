a209c2459628c3737cb2880b3ac22d81da43c1b4
Ignas Jasiunas <ignas.jasiunas@trafi.com>
2022-11-22 22:44:14 +0200

    Remove request extension debug print

--- ios-exmaple/Sources/API/Request.swift
+++ ios-exmaple/Sources/API/Request.swift
@@ -15,18 +15,6 @@ extension URLSession {
             .mapError { API.ClientError(status: $0.code.rawValue, statusCode: $0.errorCode, message: $0.localizedDescription) }
             .flatMap { response -> AnyPublisher<Response, API.ClientError> in
                 Just(response.data)
-                    .tryMap { _ in
-                        
-                        print(String(data: response.data, encoding: .utf8))
-                        
-                        do {
-                            try JSONDecoder().decode(Response.self, from: response.data)
-                        } catch {
-                            print(error)
-                        }
-                        
-                        return response.data
-                    }
                     .decode(type: Response.self, decoder: JSONDecoder())
                     .mapError {
                         ($0 as? API.ClientError) ?? .init(status: 0, statusCode: 0, message: $0.localizedDescription)
