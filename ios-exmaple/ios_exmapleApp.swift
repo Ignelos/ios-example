f2be390869b1b60b77bb47158b23f0c011b632d1
Ignas Jasiunas <ignas.jasiunas@trafi.com>
2022-11-22 19:08:45 +0200

    create project

---
+++ ios-exmaple/ios_exmapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_exmapleApp.swift
+//  ios-exmaple
+//
+//  Created by Ignas Jasiunas on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct ios_exmapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
